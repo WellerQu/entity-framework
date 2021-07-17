7454962e078143dee800e0aff51d06a95e20a4ef
qiuwei <qiuwei@bizseer.com>
2021-07-18 01:06:42 +0800

    refactor: 调整设计

--- src/annotations/object/resource.ts
+++ src/annotations/object/resource.ts
@@ -2,11 +2,13 @@ import { MetadataContext } from '../../metadata/MetadataContext'
 import { Prepare } from './Prepare'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-type ResourceDecorator = (url: string, uid: string | symbol, ResponseType?: { new(): model.DataModel }) => ClassDecorator
+type ResourceDecorator = (url: string, uid: string | symbol) => ClassDecorator
 
-export const Resource: ResourceDecorator = (url, id, ResponseType) => (target) => {
+export const Resource: ResourceDecorator = (url, id) => (target) => {
   const prepare = new Prepare(MetadataContext.instance, target)
   const entity = prepare.getEntity()
 
-  entity.setResource(id, { id, url, ResponseType })
+  entity.setResource(id, { id, url })
 }
+
+// , ResponseType?: { new(): model.DataModel }
